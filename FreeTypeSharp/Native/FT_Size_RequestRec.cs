5524932789b4c5274337e661a37593a01236c1df
Lorenzo Delana <lorenzo.delana@gmail.com>
2023-01-31 02:30:42 +0100

    added FT_Size_Request_Type enum and FT_Size_RequestRec necessary for FT_Request_Size

---
+++ FreeTypeSharp/Native/FT_Size_RequestRec.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using FT_Long = System.UInt64;
+using FT_UInt = System.UInt32;
+
+namespace FreeTypeSharp.Native
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FT_Size_RequestRec
+    {
+        public FT_Size_Request_Type type;
+        public FT_Long width;
+        public FT_Long height;
+        public FT_UInt horiResolution;
+        public FT_UInt vertResolution; 
+    }
+}
