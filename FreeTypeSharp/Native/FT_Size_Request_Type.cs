e5379dc391fe750c604d2cb38c377c81fe5881a6
Lorenzo Delana <lorenzo.delana@gmail.com>
2023-01-31 13:51:00 +0100

    added FT_Size_Request_Type enum and FT_Size_RequestRec (#21)
    * added FT_Size_Request_Type enum and FT_Size_RequestRec necessary for FT_Request_Size
    
    * specialized FT_Request_Size method arguments

---
+++ FreeTypeSharp/Native/FT_Size_Request_Type.cs
@@ -0,0 +1,13 @@
+﻿namespace FreeTypeSharp.Native
+{
+    public enum FT_Size_Request_Type
+    {
+        FT_SIZE_REQUEST_TYPE_NOMINAL = 0,
+        FT_SIZE_REQUEST_TYPE_REAL_DIM,
+        FT_SIZE_REQUEST_TYPE_BBOX,
+        FT_SIZE_REQUEST_TYPE_CELL,
+        FT_SIZE_REQUEST_TYPE_SCALES,
+
+        FT_SIZE_REQUEST_TYPE_MAX
+    }
+}
